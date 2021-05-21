042342ca36b9b2a1a6fa14f4b5221b3fd00ef6c0
Ilmir2018 <ilmir2007@inbox.ru>
2021-05-21 17:06:50 +0300

    add authorisation from sistem
    Создание начального приложения не фронте (система роутинга, авторизация, регистрация, обработка ошибок)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './classes/auth.guard';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'register', component: RegisterPageComponent}
+    ]
+  },
+  {path: '', component: SiteLayoutComponent, canActivate: [AuthGuard], children: [
+    
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TokenInterceptor } from './classes/token.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
